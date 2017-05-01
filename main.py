93553fb7ac39c97c48c140259092d9db5c49374c
nico <nicolas.p.suarez@gmail.com>
2017-05-01 16:43:30 -0300

    added fullscreen mode

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import kivy
 from kivy.app import App
+from kivy.config import Config
 
 kivy.require('1.9.1')
 
@@ -8,4 +9,5 @@ class EricApp(App):
     pass
 
 if __name__ == '__main__':
+    Config.set('graphics', 'fullscreen', 'auto')
     EricApp().run()
