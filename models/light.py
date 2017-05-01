ad1fe44e3996d787dd5ae73f5a47d2bed55741d4
nico <nicolas.p.suarez@gmail.com>
2017-05-01 18:20:14 -0300

    added light model

---
+++ models/light.py
@@ -0,0 +1,10 @@
+class Light:
+
+    TYPE_DIMMER = 'dimmer'
+    TYPE_ON_OFF = 'on_off'
+
+    def __init__(self, type):
+        if type not in [self.TYPE_DIMMER, self.TYPE_ON_OFF]:
+            raise ValueError('Light type %r is not supported' % type)
+        else:
+            self.type = type
