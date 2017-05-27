1d150455ce9a434552e38ce8ae47e47c37b583dc
nico <nicolas.p.suarez@gmail.com>
2017-05-27 18:51:43 -0300

    added PIC and SGH base commands

--- commands/basecommand.py
+++
@@ -1,7 +0,0 @@
-class BaseCommand(object):
-
-    START = bytearray([0x29])
-    END = bytearray([0, 0])
-
-    def __init__(self, node_length, params):
-        self.values = self.START + params[:node_length] + self.END
---
+++ commands/basecommands.py
@@ -0,0 +1,24 @@
+class BaseCommand(object):
+
+    START = bytearray([0x29])
+    END = bytearray([0, 0])
+
+    def __init__(self, node_len, params):
+        node = params[:node_len] + bytearray([0] * (node_len - len(params)))
+        self.values = self.START + node + self.END
+
+
+class PICCommand(BaseCommand):
+
+    NODE_LEN = 12
+
+    def __init__(self, params):
+        super(PICCommand, self).__init__(self.NODE_LEN, params)
+
+
+class SGHCommand(BaseCommand):
+
+    NODE_LEN = 200
+
+    def __init__(self, params):
+        super(SGHCommand, self).__init__(self.NODE_LEN, params)
