b61968e802d824edeb12d9ee5a72cc66459b8e25
nico <nicolas.p.suarez@gmail.com>
2017-05-28 13:27:47 -0300

    added SGH commands

---
+++ commands/sghcommands.py
@@ -0,0 +1,37 @@
+from basecommands import SGHCommand
+
+
+class Status(SGHCommand):
+    """This command is sent by the SGH"""
+    VALUE = 1
+
+
+class EricStatus(SGHCommand):
+
+    VALUE = 2
+
+    def __init__(self, plugged, rebooting):
+        super(EricStatus, self).__init__([self.VALUE, plugged, rebooting])
+
+
+class KeepAlive(SGHCommand):
+    """This command is sent by the SGH"""
+    VALUE = 3
+
+
+class StartAudioMessage(SGHCommand):
+    """This command is sent by the SGH"""
+    VALUE = 4
+
+
+class GetAccountInfo(SGHCommand):
+
+    VALUE = 5
+
+    def __init__(self):
+        super(GetAccountInfo, self).__init_([self.VALUE])
+
+
+class AccountInfo(SGHCommand):
+    """This command is sent by the SGH"""
+    VALUE = 6
