da2b51ec7aca8aeebf3e9ec7dc2ddccecf90802d
nico <nicolas.p.suarez@gmail.com>
2017-05-29 13:37:33 -0300

    added read_data() to BaseConnection

--- connections/baseconnection.py
+++ connections/baseconnection.py
@@ -1,3 +1,4 @@
+from ..commands.basecommands import BaseCommand
 from kivy.logger import Logger
 import socket
 
@@ -6,9 +7,11 @@ class BaseConnection(object):
 
     TIMEOUT_SECOUNDS = 5
 
-    def connect(self, address, port):
+    def connect(self, address, port, command_len):
         self.address = address
         self.port = port
+        self.command_len = command_len
+        self.command = []
         self.disconnect()
         Logger.info(__name__ + ': Connecting to ' +
                     self.address + ':' + self.port)
@@ -16,6 +19,14 @@ class BaseConnection(object):
             self.socket = socket.socket()
             self.socket.settimeout(self.TIMEOUT_SECOUNDS)
             self.socket.connect((address, port))
+            while True:
+                data = self.socket.recv(1024)
+                if not data:
+                    Logger.warning(': No data received from ' +
+                                   self.address + ':' + self.port)
+                    break
+                self.read_data(data)
+            self.disconnect()
         except socket.error as msg:
             Logger.warning(__name__ + ': Connection to ' + self.address + ':' +
                            self.port + ' failed with error: ' + msg)
@@ -29,7 +40,7 @@ class BaseConnection(object):
             self.socket.close()
         self.socket = None
 
-    def sendCommand(self, command):
+    def send_command(self, command):
         success = False
         if (self.socket):
             Logger.debug(__name__ + ': Sending ' + command + ' to ' +
@@ -41,4 +52,13 @@ class BaseConnection(object):
                 Logger.warning(__name__ + ': Sending ' + command + ' to ' +
                                self.address + ':' + self.port +
                                ' failed with error: ' + msg)
+                self.disconnect()
         return success
+
+    def read_data(self, data):
+        if self.command or data[:len(BaseCommand.START)] == BaseCommand.START:
+            self.command += data
+            if len(self.command) == self.command_len:
+                self.read_command()
+            elif len(self.command) > self.command_len:
+                self.command = []
