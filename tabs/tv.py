782531efbc950ca26bcd6506f82c9dd0bf2628b3
nico <nicolas.p.suarez@gmail.com>
2017-06-25 20:35:35 -0300

    added tv tab layout file

--- tabs/tv.py
+++ tabs/tv.py
@@ -2,7 +2,4 @@ from kivy.uix.tabbedpanel import TabbedPanelItem
 
 
 class TV(TabbedPanelItem):
-
-    def __init__(self, **kwargs):
-        super(TV, self).__init__(**kwargs)
-        self.text = 'TV'
+    pass
