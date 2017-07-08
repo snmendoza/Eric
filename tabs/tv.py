f1f8a0e8a0b44586f76afc4235cef7e92a1c2bfb
nico <nicolas.p.suarez@gmail.com>
2017-07-08 13:29:53 -0300

    added buttons with repeatable action

--- tabs/tv.py
+++ tabs/tv.py
@@ -3,6 +3,8 @@ from appevents import AppEvents
 from commands import piccommands
 from connections.wrappers import PICCW
 from jobq.qpool import AppQPool
+from kivy.uix.button import Button
+from kivy.clock import Clock
 from kivy.uix.tabbedpanel import TabbedPanelItem
 
 
@@ -48,3 +50,26 @@ class TV(TabbedPanelItem):
 
     def digit(self, digit):
         PICCW.send_command(piccommands.Digit(digit))
+
+
+class RepeatButton(Button):
+
+    def __init__(self, **kwargs):
+        super(RepeatButton, self).__init__(**kwargs)
+        self.register_event_type('on_repeat')
+
+    def on_repeat(self):
+        pass
+
+    def on_press(self):
+        self.lp_fired = False
+        self.clock = Clock.schedule_interval(self.on_long_press, .5)
+
+    def on_long_press(self, dt):
+        self.lp_fired = True
+        self.dispatch('on_repeat')
+
+    def on_release(self):
+        self.clock.cancel()
+        if not self.lp_fired:
+            self.dispatch('on_repeat')
