a09b6342158197738f0160470d28dd6148694843
nico <nicolas.p.suarez@gmail.com>
2017-08-11 15:55:51 -0300

    added volume adjust for audio message playback

--- players/audiomsgplayer.py
+++ players/audiomsgplayer.py
@@ -1,6 +1,7 @@
 from appconnections import PICConnection
 from appevents import Events
 from appm3s import M3S, M3SException
+from appvolumemanager import VolumeManager
 from collections import deque
 from commands import piccommands
 from models.audiomsg import AudioMsg
@@ -18,9 +19,11 @@ class AudioMsgPlayer(BasePlayer):
         Events.on_pic_start_audio_message += self.add_msg
 
     def add_msg(self, command):
-        Events.on_audio_msg_start()
         self.msg_q.append(AudioMsg(command))
         if not self.msg:
+            Events.on_audio_msg_start()
+            self.saved_volume = VolumeManager.volume
+            VolumeManager.set_volume(VolumeManager.max / 2.)
             self.next()
 
     def next(self):
@@ -34,6 +37,7 @@ class AudioMsgPlayer(BasePlayer):
                 self.on_playback_error()
         else:
             self.msg = None
+            VolumeManager.set_volume(self.saved_volume)
             Events.on_audio_msg_end()
 
     def on_playback_completed(self):
