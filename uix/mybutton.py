192f73cd04e4563a67585d0631dee08955ace2a3
nico <nicolas.p.suarez@gmail.com>
2017-09-06 01:09:39 -0300

    added background down color to all buttons

--- uix/mybutton.py
+++ uix/mybutton.py
@@ -1,14 +1,20 @@
 from kivy.uix.button import Button
-from kivy.properties import StringProperty
+from kivy.properties import ListProperty, StringProperty
 
 
 class MyButton(Button):
 
     background = StringProperty('')
+    background_down_color = ListProperty([1, 1, 1, 1])
 
     def __init__(self, **kwargs):
         super(MyButton, self).__init__(**kwargs)
         self.border = 0, 0, 0, 0
+        self.original_background_color = self.background_color
+
+    def on_background_color(self, instance, value):
+        if value != self.background_down_color:
+            self.original_background_color = value
 
     def on_background(self, instance, value):
         self.background_normal = self.background
@@ -16,3 +22,10 @@ class MyButton(Button):
         self.bakground_disabled_normal = self.background
         self.background_disabled_down = self.background
         self.height = self.width
+
+    def on_state(self, widget, value):
+        if value == 'down':
+            self.background_color = self.background_down_color
+        else:
+            # original background is just a list
+            self.background_color = self.original_background_color
